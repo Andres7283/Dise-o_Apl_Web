ad7572703b29389dfd557f269af64037c0128e35
Andres7283 <agarcia7283@ceuandalucia.es>
2025-01-22 00:54:59 +0100

    snake terminada no funciona

--- Snake js/index.js
+++ Snake js/index.js
@@ -31,17 +31,104 @@ $(document).ready(function () {
     });
   }
 
-  /*function createFood() {
+  function createFood() {
     let isFoodOnSnake;
-    do{
-        isFoodOnSnake = false;
-        food.x = Math.floor(Math.random() * (containerSize / snakeSize)) * snakeSize;
+    do {
+      isFoodOnSnake = false;
+      food.x =
+        Math.floor(Math.random() * (containerSize / snakeSize)) * snakeSize;
+      food.y =
+        Math.floor(Math.random() * (containerSize / snakeSize)) * snakeSize;
+      snake.forEach((segment) => {
+        if (segment.x === food.x && segment.y === food.y) {
+          isFoodOnSnake = true;
+        }
+      });
+    } while (isFoodOnSnake);
+    gameContainer.append(
+      `<div class = "food" style= "left: ${food.x}px; top:${food.y}px;"`
+    );
+  }
+
+  function moveSnake() {
+    let newHead = { ...snake[0] };
+
+    switch (direction) {
+      case "RIGTH":
+        newHead.x += snakeSize;
+      case "LEFT":
+        newHead.x -= snakeSize;
+      case "UP":
+        newHead.y -= snakeSize;
+      case "DOWN":
+        newHead.y += snakeSize;
     }
-  }*/
+
+    if (
+      newHead.x >= containerSize ||
+      newHead.x < 0 ||
+      newHead.y >= containerSize ||
+      newHead.y < 0 ||
+      isCollision(newHead)
+    ) {
+      clearInterval(gameInterval);
+      gameOverDisplay.show();
+      return;
+    }
+    if (newHead.x === food && newHead.y === food.y) {
+      score += 10;
+      scoreDisplay.text(`Score: ${score}`);
+      gameContainer.find(".food").remove();
+      createFood();
+      if (score % 50 === 0) {
+        speed *= 0.9;
+        clearInterval(gameInterval);
+        gameInterval = setInterval(moveSnake, speed);
+      } else {
+        snake.pop();
+      }
+      snake.unshift(newHead);
+      createSnake();
+    }
+  }
+
+  function isCollision(newHead) {
+    for (let i = 0; i < snake.length; index++) {
+      if (newHead.x === snake[i].x && newHead.y === snake[i].y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  $(document).keydown(function (e) {
+    switch (e.which) {
+      case 37:
+        if (direction !== "RIGHT") direction = "LEFT";
+        break;
+      case 38:
+        if (direction !== "DOWN") direction = "UP";
+        break;
+      case 39:
+        if (direction !== "LEFT") direction = "RIGHT";
+        break;
+      case 40:
+        if (direction !== "UP") direction = "DOWN";
+        break;
+      case 32:
+        isPaused = !isPaused;
+        if (isPaused) {
+          clearInterval(gameInterval);
+        } else {
+          gameInterval = setInterval(moveSnake, speed);
+        }
+        break;
+    }
+  });
 
   function startGame() {
     createSnake();
     createFood();
-    gameInterval = setInterval();
+    gameInterval = setInterval(moveSnake, speed);
   }
 });
